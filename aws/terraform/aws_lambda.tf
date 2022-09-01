31c4c96bf521c855a72e91b467a4e06cb6033eb3
sourcedrink <matthewbakker@hotmail.com>
2022-09-01 21:02:26 +1200

    Seperate Delete Lambda function

--- aws/terraform/aws_lambda.tf
+++ aws/terraform/aws_lambda.tf
@@ -1,24 +1,36 @@
-data "archive_file" "create-incident-archive" {
- source_file = "nodejs/backend/index.js"
- output_path = "nodejs/backend/index.zip"
+data "archive_file" "delete-incident-archive" {
+ source_file = "nodejs/backend/delete.js"
+ output_path = "nodejs/backend/delete.zip"
  type = "zip"
 }
 
-resource "aws_lambda_function" "create-incident" {
- //I have hardcoded this in the file so don't need to add an environment variable... yet
- //environment {
- //  variables = {
- //    NOTES_TABLE = aws_dynamodb_table.tf_notes_table.name
- //  }
- //}
+resource "aws_lambda_function" "delete-incident" {
  memory_size = "128"
  timeout = 10
  runtime = "nodejs14.x"
  architectures = ["arm64"]
- handler = "index.handler"
+ handler = "delete.handler"
+ function_name = "delete-incident"
+ role = aws_iam_role.iam_for_lambda.arn
+ filename = "nodejs/backend/delete.zip"
+ source_code_hash = filebase64sha256("nodejs/backend/delete.zip")
+}
+
+data "archive_file" "create-incident-archive" {
+ source_file = "nodejs/backend/create.js"
+ output_path = "nodejs/backend/create.zip"
+ type = "zip"
+}
+
+resource "aws_lambda_function" "create-incident" {
+ memory_size = "128"
+ timeout = 10
+ runtime = "nodejs14.x"
+ architectures = ["arm64"]
+ handler = "create.handler"
  function_name = "create-incident"
  role = aws_iam_role.iam_for_lambda.arn
- filename = "nodejs/backend/index.zip"
+ filename = "nodejs/backend/create.zip"
  publish = false
- source_code_hash = filebase64sha256("nodejs/backend/index.zip")
+ source_code_hash = filebase64sha256("nodejs/backend/create.zip")
 }
--- aws/terraform/nodejs/backend/create.js
+++ aws/terraform/nodejs/backend/create.js
@@ -5,21 +5,8 @@ const documentClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = async (event, context) => {
 
-  // check that the password is correct
-  if(event.body.hasOwnProperty('incidentId')) {
-    const id = event.body.incidentId;
-    await documentClient
-      .delete({
-        TableName: INCIDENT_TABLE,
-        Key: {
-          incidentId: id,
-        },
-      }).promise();
-         
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Item Deleted" })};
-  } else if (event.hasOwnProperty('body') && event.body.hasOwnProperty('password') && event.body.password == "MattAndDansSuperSecretPassword") {
+// check that the password is correct
+if (event.hasOwnProperty('body') && event.body.hasOwnProperty('password') && event.body.password == "MattAndDansSuperSecretPassword") {
 
     // create a new object
     expiryDate = Date.now() + 604800000; //This magic number is 7 days in milliseconds
