88edfa8bce65ee92c01f080652ca9500b0b8c6d9
Daniel Bakker <dbakker@diligent.com>
2022-09-08 15:34:17 +1200

    Codify the s3 bucket website part

--- aws/terraform/aws_outputs.tf
+++ aws/terraform/aws_outputs.tf
@@ -2,3 +2,8 @@ output "base_url" {
   description = "Base URL for API Gateway stage."
   value = aws_apigatewayv2_stage.lambda_stage.invoke_url
 }
+
+output "website_url" {
+  description = "URL of the frontend website"
+  value = aws_s3_bucket.b1.website_endpoint
+}
